de4fbd4a21afef2920b73700573706b898a00712
Fernando <itsfercodes@outlook.com>
2023-08-14 12:41:25 -0600

    Add toggle button

--- src/StepsApp.jsx
+++ src/StepsApp.jsx
@@ -5,8 +5,10 @@ const messages = [
   'Apply for jobs 💼',
   'Invest your new income 🤑'
 ];
+
 const StepsApp = () => {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevious = () => {
     if (step > 1) setStep(step - 1);
@@ -16,34 +18,47 @@ const StepsApp = () => {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
-      </div>
+    <>
+      <button
+        className="close"
+        type="button"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          type="button"
-          style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              type="button"
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              type="button"
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default StepsApp;
